198cc4ed7429646901e5138b4ea8c39ef54a7b70
aashishawd <41699228+aashishawd@users.noreply.github.com>
2024-06-28 04:25:39 -0700

    Final Commit

---
+++ dev_lib/calendar_agenda/lib/calendar_agenda.dart
@@ -0,0 +1,4 @@
+library calendar_agenda;
+export 'src/typedata.dart';
+export 'src/calendar.dart';
+export 'src/controller.dart';
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:budgetbuddy/common/color_extension.dart';
+import 'package:budgetbuddy/view/login/welcome_view.dart';
+import 'package:budgetbuddy/view/main_tab/main_tab_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Budget Buddy',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Inter",
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: TColor.primary,
+          background: TColor.gray70,
+          primary: TColor.primary,
+          primaryContainer: TColor.gray60,
+          secondary: TColor.secondary,
+        ),
+        useMaterial3: false,
+      ),
+      home: const MainTabView(),
+    );
+  }
+}
